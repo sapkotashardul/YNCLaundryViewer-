0ee80bb0587b3036e4ac05ce0410ac521f9454f8
Yun Da <yd.chua@u.yale-nus.edu.sg>
2020-02-24 18:23:04 +0800

    bug

--- app/process_sensor_data.py
+++ app/process_sensor_data.py
@@ -24,7 +24,7 @@ def find_latest(db, college, washer):
             t = datetime.strptime(db[college][washer][state], '%A, %I:%M:%S %p, %d-%b-%Y')
             s = state
             time = datetime.strftime(t, '%A, %I:%M:%S %p, %d-%b-%Y')
-    return(s + t)
+    return(s + time)
     #find_latest.status = s
     #find_latest.time = time
 
