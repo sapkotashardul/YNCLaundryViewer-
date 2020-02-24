20e4e4597333d2b00383f7b290406777946edb70
Yun Da <yd.chua@u.yale-nus.edu.sg>
2020-02-24 23:36:40 +0800

    setup for gsheets get

--- app/upload_gsheets.py
+++ app/upload_gsheets.py
@@ -12,39 +12,18 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = '1HHw4EQUwgcE-n5SEb7VRSkdObEtFvK3CUlYZlvj9Oy8'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
-#SERVICE_ACCOUNT_FILE = '/Users/IndieDa/Documents/GitHub/YNCLaundryViewer-/service.json'
-#creds = service_account.Credentials.from_service_account_file(
-#        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+SAMPLE_RANGE_NAME = 'Cendana!C1:D3'
+SERVICE_ACCOUNT_FILE = '/Users/IndieDa/Documents/GitHub/YNCLaundryViewer-/app/cred.json'
+credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 def main():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('sheets', 'v4', credentials=creds)
+    service = build('sheets', 'v4', credentials=credentials)
 
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
     values = result.get('values', [])
 
     if not values:
@@ -53,7 +32,8 @@ def main():
         print('Name, Major:')
         for row in values:
             # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
+            print(row)
+            #print('%s, %s' % (row[0], row[4]))
 
 if __name__ == '__main__':
     main()
